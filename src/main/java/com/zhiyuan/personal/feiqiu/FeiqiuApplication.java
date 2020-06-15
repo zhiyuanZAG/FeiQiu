eb9b85ef0d2fdbd376645c621ecd303e4eb88d80
zhiyuanzhang9 <zhiyuanzhang9@creditease.cn>
2020-06-15 19:11:00 +0800

    update ReadME

--- src/main/java/com/zhiyuan/personal/feiqiu/FeiqiuApplication.java
+++ src/main/java/com/zhiyuan/personal/feiqiu/FeiqiuApplication.java
@@ -2,12 +2,14 @@ package com.zhiyuan.personal.feiqiu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class FeiqiuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeiqiuApplication.class, args);
+		System.out.println("====server Started====");
 	}
 
 }
