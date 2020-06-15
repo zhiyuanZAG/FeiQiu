529e2ae3c3eab91b2cfbfd70ae4a05ae15801e71
zhiyuanzhang9 <zhiyuanzhang9@creditease.cn>
2020-06-15 18:06:47 +0800

    project init

---
+++ src/main/java/com/zhiyuan/personal/feiqiu/FeiqiuApplication.java
@@ -0,0 +1,13 @@
+package com.zhiyuan.personal.feiqiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiqiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeiqiuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhiyuan/personal/feiqiu/FeiqiuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhiyuan.personal.feiqiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeiqiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
